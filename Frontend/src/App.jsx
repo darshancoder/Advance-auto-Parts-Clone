50bfa9a26b1083df3b48b37205553ea81bce5f9b
Yogesh Yadav <yy072156@gmail.com>
2023-01-17 19:16:11 +0530

    Boiler Plate Ready

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import AllRoutes from "./All-Routes/AllRoutes";
+
+/*
+Dont make any changes to this file
+*/
+
+function App() {
+  return (
+    <>
+      <AllRoutes />
+    </>
+  );
+}
+
+export default App;
